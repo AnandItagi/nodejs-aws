fb10bcd2a19cf941ca04b1f1258182957049f303
AnandItagi <aitagi7@gmail.com>
2024-03-03 23:14:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 app.get("/", (req, res) => res.send("Hello world dockerised"));
 app.listen(80, () => {
-  console.log("listening on port 3000");
+  console.log("listening on port 80");
 });
